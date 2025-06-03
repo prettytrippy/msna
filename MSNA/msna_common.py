cf87fd24732d381594f6db4a239d176845705433
prettytrippy <trippdow@gmail.com>
2025-06-03 11:30:51 -0500

    Change metrics to Ryan's version

--- MSNA/msna_common.py
+++ MSNA/msna_common.py
@@ -1,11 +1,102 @@
 import glob
 import numpy as np
-from scipy.signal import butter, filtfilt, medfilt, find_peaks
+from scipy.signal import butter, filtfilt
 from scipy.stats import rankdata
-from numpy.lib.stride_tricks import sliding_window_view
 import pandas as pd
 from tqdm import tqdm
-from sklearn.metrics import roc_auc_score
+from typing import Tuple
+
+def msna_metric(
+    pred_peaks: np.ndarray, true_peaks: np.ndarray, window_size: int = 40
+) -> Tuple[float, float, float]:
+    """
+    An F1 metric based on a custom ground-truth binning scheme for this task. 
+    
+    Args:
+        pred_peaks (np.ndarray): An array of integers representing where each 
+            MSNA peak was found.
+        true_peaks (np.ndarray): An array of integers representing where each 
+            ground truth MSNA peak is. This can be computed as `peaks_from_bool_1d(df["Burst"])`
+        window_size (int): The size of the window around each true peak.
+    
+    Returns:
+        float: The F1 score. 
+    """
+    pred, true = bin_predictions(pred_peaks, true_peaks, window_size)
+    return scores(pred, true)
+
+def bin_predictions(
+    pred_peaks: np.ndarray, true_peaks: np.ndarray, window_size: int = 40
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Custom binning scheme for the predicted and ground truth data. For each
+    ground truth peak, we take some window around it and consider that as the
+    true range. Any time-point that is not apart of this window is considered a
+    false section. In this sense, this metric is NOT commutative and would be
+    very sensitive to swapping the pred and true peaks. 
+
+    Args:
+        pred_peaks (np.ndarray): An array of integers representing where each 
+            MSNA peak was found.
+        true_peaks (np.ndarray): An array of integers representing where each 
+            ground truth MSNA peak is. This can be computed as `peaks_from_bool_1d(df["Burst"])`
+        window_size (int): The size of the window around each true peak.
+    
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: The pred and true binned data (in this order).
+    """
+    l = true_peaks - window_size
+    r = true_peaks + window_size
+
+    pred = []
+    true = []
+    for i in range(len(l)):
+        # Check if there's at least one prediction in this window
+        if np.sum((pred_peaks >= l[i]) & (pred_peaks < r[i])) >= 1: 
+            pred.append(True)
+            true.append(True)
+        else:
+            pred.append(False)
+            true.append(True)
+            
+        # Check gap to next peak (only if not the last peak)
+        if i < len(l) - 1:
+            if np.sum((pred_peaks >= r[i]) & (pred_peaks < l[i+1])) >= 1: 
+                pred.append(True)
+                true.append(False)
+            else:
+                pred.append(False)
+                true.append(False)
+    
+    return np.array(pred), np.array(true)
+
+def scores(pred: np.ndarray, true: np.ndarray) -> Tuple[float, float, float]:
+    """An F1 metric based on boolean `pred` and `true` aligned data."""
+    if np.array(pred.shape) != np.array(true.shape):
+        raise ValueError("`pred` and `true` arrays must have the same shape.")
+
+    tp, fp, tn, fn = confusion_matrix_values(pred, true)
+    
+    recall = tp / (tp + fn)
+    precision = tp / (tp + fp)
+    f1 = (2 * precision * recall) / (precision + recall)
+    
+    return f1, precision, recall
+ 
+def confusion_matrix_values(
+    pred: np.ndarray, true: np.ndarray
+) -> Tuple[float, float, float, float]:
+    """Get the true/false positive/negatives of the boolean data."""
+    tp = np.sum((pred == 1) & (true == 1))
+    fp = np.sum((pred == 1) & (true == 0))
+    tn = np.sum((pred == 0) & (true == 0))
+    fn = np.sum((pred == 0) & (true == 1))
+    
+    return tp, fp, tn, fn
+
+def peaks_from_bool_1d(bool_array: np.ndarray) -> np.ndarray: 
+    """Converts a boolean array to an array of integer indices."""
+    return np.where(bool_array)[0]
 
 def get_burst_idxs(msna):
     return np.nonzero(msna['BURST'].to_numpy())[0]
@@ -242,46 +333,64 @@ def new_stats(self, df, actual_bursts, predicted_bursts):
 def metrics(self, df, verbose=False):
         actual_bursts = self.get_burst_idxs(df)
         predicted_bursts = self.predict_labels(df)
+
+        f1, precision, recall = msna_metric(predicted_bursts, actual_bursts)
         
-        stats = new_stats(self, df, actual_bursts, predicted_bursts)
-        if stats == None:
-            return 0.0, 0.0, 0.0
+        # stats = new_stats(self, df, actual_bursts, predicted_bursts)
+        # if stats == None:
+        #     return 0.0, 0.0, 0.0
             
-        TP, FP, TN, FN = stats
+        # TP, FP, TN, FN = stats
+    
+        # # Calculate Precision, Recall, F1-score, and Accuracy
+        # precision = TP / (TP + FP) if (TP + FP) > 0 else 0
+        # recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+        # f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+        # accuracy = (TP + TN) / (TP + FP + FN + TN) if (TP + FP + FN + TN) > 0 else 0
+    
+        return (precision, recall, f1)
         
-        # Calculate Precision, Recall, F1-score, and Accuracy
-        precision = TP / (TP + FP) if (TP + FP) > 0 else 0
-        recall = TP / (TP + FN) if (TP + FN) > 0 else 0
-        f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
-        accuracy = (TP + TN) / (TP + FP + FN + TN) if (TP + FP + FN + TN) > 0 else 0
+        # stats = new_stats(self, df, actual_bursts, predicted_bursts)
+        # if stats == None:
+        #     return 0.0, 0.0, 0.0
+            
+        # TP, FP, TN, FN = stats
+        
+        # # Calculate Precision, Recall, F1-score, and Accuracy
+        # precision = TP / (TP + FP) if (TP + FP) > 0 else 0
+        # recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+        # f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+        # accuracy = (TP + TN) / (TP + FP + FN + TN) if (TP + FP + FN + TN) > 0 else 0
     
-        if verbose:
-            print(f"True Positives (TP): {TP}")
-            print(f"False Positives (FP): {FP}")
-            print(f"False Negatives (FN): {FN}")
-            print(f"True Negatives (TN): {TN}")
-            print(f"Precision: {precision:.2f}")
-            print(f"Recall: {recall:.2f}")
-            print(f"F1-score: {f1_score:.2f}")
-            print(f"Accuracy: {accuracy:.2f}")
+        # if verbose:
+        #     print(f"True Positives (TP): {TP}")
+        #     print(f"False Positives (FP): {FP}")
+        #     print(f"False Negatives (FN): {FN}")
+        #     print(f"True Negatives (TN): {TN}")
+        #     print(f"Precision: {precision:.2f}")
+        #     print(f"Recall: {recall:.2f}")
+        #     print(f"F1-score: {f1_score:.2f}")
+        #     print(f"Accuracy: {accuracy:.2f}")
     
-        return (precision, recall, f1_score)
+        # return (precision, recall, f1_score)
 
 def training_metrics(self, df, probabilities, possible_burst_indices):
 
         actual_bursts = self.get_burst_idxs(df)
         predicted_bursts = self.threshold_probabilities(probabilities, possible_burst_indices)
+
+        f1, precision, recall = msna_metric(predicted_bursts, actual_bursts)
         
-        stats = new_stats(self, df, actual_bursts, predicted_bursts)
-        if stats == None:
-            return 0.0, 0.0, 0.0
+        # stats = new_stats(self, df, actual_bursts, predicted_bursts)
+        # if stats == None:
+        #     return 0.0, 0.0, 0.0
             
-        TP, FP, TN, FN = stats
+        # TP, FP, TN, FN = stats
     
-        # Calculate Precision, Recall, F1-score, and Accuracy
-        precision = TP / (TP + FP) if (TP + FP) > 0 else 0
-        recall = TP / (TP + FN) if (TP + FN) > 0 else 0
-        f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
-        accuracy = (TP + TN) / (TP + FP + FN + TN) if (TP + FP + FN + TN) > 0 else 0
+        # # Calculate Precision, Recall, F1-score, and Accuracy
+        # precision = TP / (TP + FP) if (TP + FP) > 0 else 0
+        # recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+        # f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+        # accuracy = (TP + TN) / (TP + FP + FN + TN) if (TP + FP + FN + TN) > 0 else 0
     
-        return (precision, recall, f1_score)
+        return (precision, recall, f1)
